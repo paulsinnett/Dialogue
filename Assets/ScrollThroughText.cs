4bdc3c755e492f94a01392e2e5d91d5b14e288e3
Paul Sinnett <paul.sinnett@gmail.com>
2017-05-11 17:43:56 +0100

    simple dialogue captions and audio

---
+++ Assets/ScrollThroughText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollThroughText : MonoBehaviour {
+	private int m_currentNo;
+	public Text[] textBoxes;
+	public AudioClip[] audioClips;
+	public AudioSource audioSource;
+
+	void Start() 
+	{
+		m_currentNo = -1;
+		ScrollText();
+	}
+
+	public void ScrollText() 
+	{
+		m_currentNo++;
+		audioSource.Stop();
+		for (int i = 0; i < textBoxes.Length; ++i) 
+		{
+			textBoxes[i].gameObject.SetActive(i == m_currentNo);
+		}
+		if (m_currentNo < audioClips.Length) 
+		{
+			audioSource.clip = audioClips [m_currentNo];
+			audioSource.Play ();
+		}
+	}
+}
